1382d22ed152d08429134c6373031a4f48105ec2
Marco-Cesar <viegascesar350@gmail.com>
2025-03-20 14:39:30 -0400

    Add files via upload

---
+++ Algo/ex3.py
@@ -0,0 +1,6 @@
+numero1=int(input("digite um numero"))
+numero2=int(input("digite outro numero"))
+soma= numero1+numero2
+subtação= numero1-numero2
+multiplicação= numero1*numero2
+print(f" soma={soma} subtaçao={subtação} multiplicação={multiplicação}")
---
+++ Algo/index.py
@@ -0,0 +1,14 @@
+# print("mensagem")      
+# #isso e um comemtario
+
+# idade= 6
+# altura= 0.50
+# peso= 5.950
+# nome="francisco"
+# print(idade, altura, peso, nome)
+
+# int=intero float=real string=caractere
+# nome = input("insira seu nome:")        #o input para usar o escreva
+# idade = int(input("insira sua idade:"))
+# peso = float(input("insira seu peso:"))
+# print(f"{nome}{idade}anos{peso}kg")    #f= fução de fomatação para usa varias variaves 
